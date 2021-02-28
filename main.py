9353cef0ac45e0c6f812d694fa17e0a63d3ac7b9
IGOR <xokvvpvlj6i3@mail.ru>
2021-02-28 22:15:19 +1000

    Дорабатываем функцию записи в TXT фацл

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
+import glob
 import json
 import os
 import requests
-
 
 # Your API_KEY must be specified here
 api_key = os.environ["YT_API_KEY"]
 
 
-def save_to_json_file(file_name: str, data):
+def save_to_json_file(file_name: str, data: dict):
     with open(file_name, "w") as f:
         json.dump(data, f, indent=4)
 
 
-def save_to_txt_file(file_name, data):
+def save_to_txt_file(file_name: str, data: dict):
     """
     Функция сохраняет результаты работы скрипта в текстовый файл.
 
     Parameters:
-        file_name - (string) Имя сохраняемого файла
+        file_name - (string) Имя файла для сохранения искомых данных
         data - (Dict) Словарь, из которого извлекаем для записи данные
     Returns:
         ???
     """
-    for count, item in enumerate(data):
+
+    # Пробегаемся по всем 50 элементам (саписям о видосах)
+    for count, item in enumerate(data["items"]):
         try:
             link = f'https://www.youtube.com/watch?v={item["id"]["videoId"]}'
         except Exception:
@@ -31,7 +33,7 @@ def save_to_txt_file(file_name, data):
         channel = item["snippet"]["channelTitle"]
         title = item["snippet"]["title"]
 
-        with open("videos.txt", "a", encoding="utf-8") as file:
+        with open(file_name, "a", encoding="utf-8") as file:
             file.write(f"{count + 1}) {channel}-{title}\n{link}\n")
 
 
@@ -57,28 +59,31 @@ def fetch_all_channel_videos(channel_url: str):
     channel_id = channel_url.split("/")[-1]
     channel_url = api_url + f"&channelId={channel_id}"
 
-    page_token = None
     index = 1
+    page_token = None
     while True:
         next_page_token = f"&pageToken={page_token}" if page_token else ''
+        channel_url = channel_url + next_page_token
 
         res = requests.get(channel_url, params=params)
         data = res.json()
 
         save_to_json_file(f"json/videos_{index}.json", data)
+        print(f"Файл: json/videos_{index}.json записан!")
 
-    # nextPageToken = data.get('nextPageToken')
-    #
-    # while nextPageToken:
-    #     response = requests.get(url + f"&pageToken={nextPageToken}")
-    #     data["items"] += response.json()["items"]
-    #
-    #     if "nextPageToken" in response:
-    #         nextPageToken = True
-    #     else:
-    #         nextPageToken = False
+        index += 1
+
+        page_token = data.get("nextPageToken")
+        if not page_token:
+            return True
 
 
 if __name__ == '__main__':
-    url = "https://www.youtube.com/channel/UCbXgNpp0jedKWcQiULLbDTA"
-    fetch_all_channel_videos(url)
+    # url = "https://www.youtube.com/channel/UCbXgNpp0jedKWcQiULLbDTA"
+    # fetch_all_channel_videos(url)
+
+    files = glob.glob("json/*.json")
+    for file in files:
+        with open(file, "r") as f:
+            info = json.loads(f.read())
+        save_to_txt_file("videos.txt", info)
